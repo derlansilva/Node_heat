6300099433e7b2009f526eeb73133ad7f47057c7
unknown <joderlan.silva@gmail.com>
2022-02-01 23:42:41 -0300

    retornando os dados do usuario  do gitHub

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
    1 https://github.com/settings/developer e configurar 
 [] #8 configurar url de redirecionamento do github
 [] #9 yarn add axios // para usar uma api externa
+[] #10 yarn add jsonwebtoken
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 /**
  * Receber code(string)
  * Recuperar acces_token  no github
+ * Recuperar info do users no github
  * Verificar se o usuario existe no banco de dados
  *--------SIM  = Gera um token
  *--------NÂO  = Cria no DB gera um token
@@ -10,14 +11,26 @@
 
 import axios from 'axios'
 
+interface IAccesTokenResponse{
 
-//
+    access_token : string
+
+}
+
+interface IUserResponse{
+    avartar_url : string,
+    login : string,
+    id: number,
+    name : string
+}
+
+
 class AuthenticateUserService{
  
     async execute(code:string){
         const url = "https://github.com/login/oauth/access_token"
 
-        const response = await axios.post(url , null , {
+        const { data : accessTokenResponse} = await axios.post<IAccesTokenResponse>(url , null , {
             params: {
                 client_id : process.env.GITHUB_CLIENT_ID , 
                 client_secret : process.env.GITHUB_CLIENT_SECRET,
@@ -25,6 +38,12 @@ class AuthenticateUserService{
             },
             headers: {
                 "Accept" : "application/json"
+            }
+        })
+
+        const response = await  axios.get<IUserResponse>("https://api.github.com/user" , {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
             }
         })
 
